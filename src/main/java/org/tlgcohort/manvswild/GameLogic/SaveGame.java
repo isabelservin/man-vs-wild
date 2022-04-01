5d22b85c037210989c531c8713a24b19a80b6d11
David Ledesma <davidled97@gmail.com>
2022-04-01 16:15:24 -0700

    updated locations, AB#184

--- src/main/java/org/tlgcohort/manvswild/GameLogic/SaveGame.java
+++ src/main/java/org/tlgcohort/manvswild/GameLogic/SaveGame.java
@@ -8,7 +8,6 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class SaveGame{
-    private static final String LOCATION_PATH = "src/main/resources/Locations.json";
     public void saving(Player player){
         try {
             ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("savedGame"));
